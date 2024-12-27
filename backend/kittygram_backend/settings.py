8afc4b32d8112e0f7c439a81dcb46cfc680e4f4d
Vladislav Gorban <clarkefeed1@gmail.com>
2024-12-27 15:51:47 +0000

    fixes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.151.157']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.151.157']
 
 STATIC_ROOT = "/app/collected_static"
 
