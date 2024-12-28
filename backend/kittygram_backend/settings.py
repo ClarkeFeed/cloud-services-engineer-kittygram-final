917b596df892dbb287f880a9128be09bddda8a17
Vladislav Gorban <clarkefeed1@gmail.com>
2024-12-28 13:16:37 +0000

    small change in post review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
